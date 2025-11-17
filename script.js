7ab33acc2f6147228ab57ee22dfd2e906cbddf7a
cauakoo <demetrocaua3@gmail.com>
2025-11-17 20:31:28 +0000

    jogo de pedra papel tesoura

---
+++ script.js
@@ -0,0 +1,69 @@
+let  pontosHumano = 0; 
+let pontosRobo = 0;
+ 
+function escolhacomputador(){
+    const numero = Math.random();
+    if (numero < 1/3 ){
+        
+    return "pedra"
+    }else if (numero < 2/3){
+        return "papel"
+    } else {
+        return "tesoura"
+    }
+    
+}
+console.log(escolhacomputador());
+
+
+
+function jogarrodada (humano,computador){
+    humano = humano.toLowerCase();
+    if (humano == computador){
+        return "empatou ambos escolheram "+ humano 
+    }
+
+
+
+    if (humano == "pedra" && computador == "tesoura" ||
+        humano == "papel" && computador == "pedra" ||
+        humano == "tesoura" && computador == "papel"
+    ){
+        pontosHumano++
+        return  "vc venceu " + humano + " vence de " + computador;
+    } else {
+        pontosRobo++ 
+        return " vc perdeu " + computador + " ganha de " + humano;
+    }
+    
+}
+
+
+
+document.getElementById("botao").addEventListener("click", function(){
+
+
+
+
+    const jogador = document.getElementById("escolhajogador").value;
+    const computador = escolhacomputador();
+    const resultado = jogarrodada(jogador, computador);
+
+
+    if (pontosHumano >= 3) {
+        document.getElementById("resultado").textContent = "voce ganhou. reiniciando a partida.";
+        document.getElementById("pontoshumano").textContent = 3;
+        pontosHumano = 0;
+        pontosRobo = 0;
+    } else if (pontosRobo >= 3) {
+        document.getElementById("resultado").textContent = "voce perdeu. reiniciando a partida";
+        document.getElementById("pontoscomputador").textContent = 3;
+        pontosHumano = 0;
+        pontosRobo = 0;
+    }   else {
+        document.getElementById("resultado").textContent = resultado;
+        document.getElementById("pontoshumano").textContent = pontosHumano;
+        document.getElementById("pontoscomputador").textContent = pontosRobo;
+    }
+    
+})
